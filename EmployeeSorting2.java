c313772359040322f770f9091181b1ef7af21a3b
chaitanya2510 <105356161+chaitanya2510@users.noreply.github.com>
2022-05-11 16:17:07 +0530

    Create EmployeeSorting2.java
    sorting the employees

---
+++ EmployeeSorting2.java
@@ -0,0 +1,58 @@
+package com.employeelist;
+
+
+
+public class Employee implements Comparable<Employee>{
+  private String name;
+  
+	private int id;
+	
+	private double salary;
+	
+	public Employee(int id, String name, double salary) {
+		super();
+    this.name = name;
+		this.id = id;
+		this.salary = salary;
+	}
+  public String getName() {
+		return name;
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	
+	public double getSalary() {
+		return salary;
+	}
+
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	 public int compareTo(Employee o) {
+	  return this.getName().compareTo(o.getName());
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
