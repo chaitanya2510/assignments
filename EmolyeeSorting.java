bcf8676d37d529c16859a60639a8d1c88773da17
chaitanya2510 <105356161+chaitanya2510@users.noreply.github.com>
2022-05-11 16:03:39 +0530

    Create EmolyeeSorting.java

---
+++ EmolyeeSorting.java
@@ -0,0 +1,25 @@
+package com.employee;
+import java.util.*;
+
+public class Employee{
+
+    public static void main(String[] args){
+
+    
+    String str[] ={"Chaitanya","Sushant","Ram","Vaibhav","Kirti"};
+    String temp;
+    System.out.println("Strings in sorted order");
+    for (int j=0; j< str.length; j++){
+        for(int i= j+1; i< str.length; i++){
+            if(str[i].compareTo(str[j])<0){
+                temp =str[j];
+                str[j]=str[i];
+                str[i]=temp;
+
+            }
+        }
+        System.out.println(str[j]);
+    }
+
+    }
+}
