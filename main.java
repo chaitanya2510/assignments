5a8d9b6701d6d417da1f451f948f5e575be02cf9
chaitanya2510 <105356161+chaitanya2510@users.noreply.github.com>
2022-05-17 21:33:22 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,42 @@
+package insurancebazaar;
+
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) 
+	{
+    
+    
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no of names you want to store:");
+		int n=sc.nextInt();
+		Insurancebazaar b=new Insurancebazaar();
+		for(int i=0;i<n;i++) {
+			System.out.println("Enter the PoliceID:");
+			int pid=sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter the policy Name:");
+			String name=sc.nextLine();
+			b.addPolicyDetails(pid,name);
+		}
+    
+		Set<Entry<Integer,String>> eset=b.getPolicyMap().entrySet();
+		for(Entry<Integer,String> e:eset) {
+			System.out.println(e.getKey()+" "+e.getValue());
+		}
+    
+		System.out.println("Enter the policy type to search");
+		String key=sc.next();
+		List<Integer>policies =b.searchBasedOnPolicyType(key);
+		for(Integer p:policies) {
+			System.out.println(p);
+      
+		}
+
+	}
+
+}
+
